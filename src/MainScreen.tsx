d2e8434bb6fbcea0974fde7389f4f29a3d285ac6
dekim1028 <daeun235194@gmail.com>
2022-09-07 12:30:57 +0900

    ✨

---
+++ src/MainScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MainScreen = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text>MainScreen</Text>
+        </View>
+    );
+};
+
+export default MainScreen;
