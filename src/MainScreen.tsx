176f59e1658daf3312291c25a0ed7ee46b199e53
dekim1028 <daeun235194@gmail.com>
2022-09-13 11:26:41 +0900

    ✨ : 플레이어 테스트

--- src/MainScreen.tsx
+++ src/MainScreen.tsx
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    Text,
+    Touchable,
+    TouchableOpacity,
+    View
+} from 'react-native';
 import Video from 'react-native-video';
 
 const MainScreen = () => {
+    const [isPaused, setIsPaused] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [rate, setRate] = useState(1);
+    const videoRef = useRef<Video>(null);
+
     const [url] = useState(
         'https://d10qaatmmd1f48.cloudfront.net/dD5wGeZdHjoS/2022/1/11/6/6/HtsvZpodLas7/media/hls/360p30/playlist.m3u8'
     );
@@ -14,12 +25,74 @@ const MainScreen = () => {
         <View style={{ flex: 1 }}>
             <Text>MainScreen</Text>
             <Video
+                ref={videoRef}
                 source={{ uri: `${url}?token=${playToken}` }}
                 style={{ width: '100%', height: 200 }}
-                resizeMode="none"
+                resizeMode="cover"
+                paused={isPaused}
+                rate={rate}
+                ignoreSilentSwitch="ignore"
+                onProgress={(e) => setCurrentTime(e.currentTime)}
             />
+            <Text>현재 : {Math.floor(currentTime)}</Text>
+            <TouchableOpacity
+                style={styles.btnStyle}
+                onPress={() => setIsPaused(!isPaused)}
+            >
+                <Text>{isPaused ? '재생' : '일시정지'}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.btnStyle}
+                onPress={() => {
+                    if (videoRef.current) {
+                        videoRef.current.seek(currentTime + 10);
+                    }
+                }}
+            >
+                <Text>10초 앞으로</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.btnStyle}
+                onPress={() => {
+                    if (videoRef.current) {
+                        videoRef.current.seek(currentTime - 10);
+                    }
+                }}
+            >
+                <Text>10초 뒤로</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.btnStyle}
+                onPress={() => setRate(1)}
+            >
+                <Text>1 배속</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.btnStyle}
+                onPress={() => setRate(2)}
+            >
+                <Text>2 배속</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.btnStyle}
+                onPress={() => {
+                    if (videoRef.current) {
+                        videoRef.current.presentFullscreenPlayer();
+                    }
+                }}
+            >
+                <Text>전체 화면</Text>
+            </TouchableOpacity>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    btnStyle: {
+        marginVertical: 5,
+        padding: 10,
+        backgroundColor: 'yellow'
+    }
+});
 
 export default MainScreen;
